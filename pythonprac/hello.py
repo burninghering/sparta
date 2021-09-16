774677f8b54c8d94c99a0e19d66f8f31aea16d2d
burninghering <37091602+burninghering@users.noreply.github.com>
2021-09-17 00:49:26 +0900

    크롤링 연습/DB 연습 시작

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,9 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+#크롤링하기
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-# 코딩 시작
+trs=soup.select('#old_content > table > tbody > tr')
+#old_content > table > tbody > tr:nth-child(2) > td.title > div > a
+#old_content > table > tbody > tr:nth-child(2)
+for tr in trs:
+    a_tag=tr.select_one('td.title > div > a')
+    if a_tag is not None:
+        rank=tr.select_one('td:nth-child(1) > img')['alt']
+        title=a_tag.text
+        star=tr.select_one('td.point').text
+        print(rank,title,star)
+
+
